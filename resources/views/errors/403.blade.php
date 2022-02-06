5a37fa2052dd5aa4f6dc4099a78c78d44ff8e5b6
caffi <caffi.senna@gmail.com>
2022-02-06 15:56:11 +0900

    Update 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,6 @@
 @extends('errors::minimal')
 
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('title', __('403 | Forbidden'))
+@section('code', '')
+{{-- @section('message', __($exception->getMessage() ?: 'Forbidden')) --}}
+@section('message','会場でお待ちしてます')

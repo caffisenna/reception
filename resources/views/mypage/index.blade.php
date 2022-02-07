fba4e2a2d32dd6c9e332a963ba420cd06dd287eb
caffi <caffi.senna@gmail.com>
2022-02-07 10:09:29 +0900

    Update index.blade.php
    edit layout

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -35,8 +35,8 @@
 
                     <div class="card">
                         <div style="background-color:#115740">
-                        <p class="uk-text-large uk-text-center" style="color:#FFF">100周年レセプション<br>デジタルパス</p>
-                    </div>
+                            <p class="uk-text-large uk-text-center" style="color:#FFF">100周年レセプション<br>デジタルパス</p>
+                        </div>
                         <p class="uk-text-primary uk-text-small">このページをブックマーク、もしくはスクリーンショットを保存して受付でご提示下さい。</p>
                         <div class="card-body p-0">
                             <table class="uk-table uk-table-hover uk-table-striped">
@@ -45,25 +45,21 @@
                                     <td>{{ $participant->name }} 様</td>
                                 </tr>
                                 <tr>
+                                    <td>QR</td>
+                                    <td>{!! QrCode::size(200)->generate(url('/?id=') . $participant->uuid) !!}</td>
+                                </tr>
+                                <tr>
                                     <td>座席番号</td>
                                     <td>{{ $participant->seat_number }}</td>
                                 </tr>
                                 <tr>
-                                    <td>県連盟</td>
-                                    <td>{{ $participant->pref }}</td>
-                                </tr>
-                                <tr>
                                     <td>所属</td>
-                                    <td>{{ $participant->district }} {{ $participant->dan_name }}
-                                        {{ $participant->dan_number }}</td>
+                                    <td>{{ $participant->pref }}連盟 @if ($participant->district){{ $participant->district }}地区@endif
+                                        {{ $participant->dan_name }}@if($participant->dan_number){{ $participant->dan_number }}団@endif</td>
                                 </tr>
                                 <tr>
                                     <td>役務</td>
                                     <td>{{ $participant->role }}</td>
-                                </tr>
-                                <tr>
-                                    <td>QR</td>
-                                    <td>{!! QrCode::size(200)->generate(url('/?id=').$participant->uuid) !!}</td>
                                 </tr>
                             </table>
                         </div>

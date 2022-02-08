3cdfa9f7dd6d4b7e6711d6565cc825effaeddee9
caffi <caffi.senna@gmail.com>
2022-02-08 13:38:04 +0900

    Update home.blade.php
    welcome messageとリンク

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-
+    <div class="container">
+        <div class="row">
+            @auth
+                <h3>スタッフさんようこそ</h3>
+                <ul class="uk-list">
+                    <li>スマホのカメラを起動して来場者のQRコードをスキャンしてください</li>
+                    <li>もしエラーで読み取れない場合は↓のボタンから手作業でチェックイン!</li>
+                </ul>
+                <a href="{{ url('/s/check_in/input') }}"
+                    class="uk-button uk-button-primary uk-button-xlarge form-control">手入力</a>
+            @endauth
+        </div>
     </div>
-</div>
 @endsection

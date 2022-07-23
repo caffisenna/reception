24cd64e4ce689419f231eabce811c26c77da79e5
caffi <caffi.senna@gmail.com>
2022-07-23 18:53:32 +0900

    participantsにカラム追加#10
    close #10

--- database/migrations/2022_02_06_025049_create_participants_table.php
+++ database/migrations/2022_02_06_025049_create_participants_table.php
@@ -23,6 +23,14 @@ class CreateParticipantsTable extends Migration
             $table->string('dan_name')->nullable();
             $table->string('dan_number')->nullable();
             $table->string('role')->nullable();
+            $table->integer('pref_priority')->nullable();       // 県連優先順位
+            $table->boolean('is_represent')->default(false);    // 県連代表
+            $table->boolean('is_proxy')->default(false);        // 代理 0 1
+            $table->boolean('wheel_chair')->default(false);     // 車椅子 0 1
+            $table->string('with_helper')->nullable();          // 介助者同行 ID
+            $table->string('go_with_leader')->nullable();       // 同行親リーダー メアド or uuid
+            $table->string('go_with_scouts')->nullable();       // 引率されるスカウト uuid
+            $table->datetime('self_absent_at')->nullable();     // セルフ欠席処理 日時
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('seat_number')->nullable();

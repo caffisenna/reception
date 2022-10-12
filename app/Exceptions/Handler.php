cbcd9d01a4d921f1b46c4d73f31e05ee1d7fcfea
caffi <caffi.senna@gmail.com>
2022-10-12 15:59:00 +0900

    feature#53
    close #53
    
    単純にログイン状態だとログイン画面にリダイレクトされるけど、編集などをしているとだめだ。(csrfトークン絡み?)

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+// use Illuminate\Session\TokenMismatchException;
+use Illuminate\Session\TokenMismatchException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +39,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        // Tokenエラーの時、ログイン画面にリダイレクトする。
+        if ($exception instanceof TokenMismatchException) {
+            return redirect(route('login'));
+        }
+
+        return parent::render($request, $exception);
     }
 }

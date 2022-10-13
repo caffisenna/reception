b4fa437cc31e023a9715ce8a1a720a622eb04869
caffi <caffi.senna@gmail.com>
2022-10-13 21:33:13 +0900

    feature#55
    close #55

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\Participant;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (isset(Auth::user()->is_pref)) {
+            // 県連認証済みなら一覧にリダイレクト
+            $participants = Participant::where('pref', Auth::user()->is_pref)->where('is_represent', '<>', '任意参加者')->paginate(100);
+            return view('pref_participants.index')->with('participants', $participants);
+        } else {
+            // それ以外はhomeへ
+            return view('home');
+        }
     }
 }

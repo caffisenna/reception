2ff959cc880704896d49ca837af5c4367f2b2deb
caffi <caffi.senna@gmail.com>
2022-10-31 10:30:25 +0900

    テーブルやめた

--- resources/views/participants/sendmail_pref.blade.php
+++ resources/views/participants/sendmail_pref.blade.php
@@ -19,26 +19,16 @@
 
         <div class="card">
             <div class="card-body p-0">
-                <div class="table-responsive">
-                    <table class="uk-table uk-table-divider" id="participants-table">
-                        <thead>
-                            <tr>
-                                <th>送信</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($prefs as $pref)
-                                <tr>
-                                    <td>
-                                        <a href="{{ url('/admin/sendmail_pref') . '/?pref=' }}{{ $pref }}"
-                                            onclick="return confirm('{{ $pref }}連盟へデジタルパスを送信します。よろしいですか？')"
-                                            class="uk-button uk-button-primary uk-width-1-6"><span uk-icon="mail">{{ $pref }}</span></a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                <ul class="uk-list">
+                    @foreach ($prefs as $pref)
+                        <li>
+                            <a href="{{ url('/admin/sendmail_pref') . '/?pref=' }}{{ $pref }}"
+                                onclick="return confirm('{{ $pref }}連盟へデジタルパスを送信します。よろしいですか？')"
+                                class="uk-button uk-button-primary uk-width-1-6"><span
+                                    uk-icon="mail">{{ $pref }}</span></a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>

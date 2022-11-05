0b98bfcf21e8ad43e2e656478a7920927c2cd876
caffi <caffi.senna@gmail.com>
2022-11-06 00:00:23 +0900

    Update table.blade.php

--- resources/views/participants/table.blade.php
+++ resources/views/participants/table.blade.php
@@ -4,9 +4,9 @@
             <tr>
                 <th>県連</th>
                 <th>役務</th>
-                <th>代理役務</th>
                 <th>氏名</th>
                 <th>座席</th>
+                <th>参加費</th>
                 <th>操作</th>
             </tr>
         </thead>
@@ -17,33 +17,36 @@
                     <td>
                         @switch($participant->category)
                             @case('県連代表(1)')
-                                理事長
+                                <span class="uk-text-success">理事長</span>
                             @break
 
                             @case('県連代表(2)')
-                                県コミッショナー
+                                <span class="uk-text-success">県コミッショナー</span>
                             @break
 
                             @case('県連代表(3)')
-                                事務局長
+                                <span class="uk-text-success">事務局長</span>
                             @break
 
                             @case('県連代表(4)')
-                                引率指導者
+                                <span class="uk-text-success">引率指導者</span>
                             @break
 
                             @case('県連代表(5)')
-                                VSスカウト
+                                <span class="uk-text-success">VSスカウト</span>
                             @break
 
                             @case('県連代表(6)')
-                                BSスカウト
+                                <span class="uk-text-success">BSスカウト</span>
                             @break
 
                             @default
+                                任意参加者
                         @endswitch
+                        @if (isset($participant->is_proxy) && $participant->is_proxy !== '')
+                            <br><span class="uk-text-small">(代理:{{ $participant->is_proxy }})</span>
+                        @endif
                     </td>
-                    <td>{{ $participant->is_proxy }}</td>
                     <td><a href="{{ route('participants.show', [$participant->id]) }}">{{ $participant->name }}</a>
                         <span class="uk-text-warning">
                             @if (isset($participant->vs))
@@ -54,8 +57,20 @@
                             @endif
                         </span>
                     </td>
-                    <td>{{ $participant->seat_number }}</td>
-                    <td width="120">
+                    <td>
+                        @if (isset($participant->seat_number))
+                            式典:{{ $participant->seat_number }}
+                        @endif
+                        @if (isset($participant->reception_seat_number))
+                            <br>レセ:{{ $participant->reception_seat_number }}
+                        @endif
+                    </td>
+                    <td>
+                        @if (isset($participant->fee_checked_at))
+                            済み
+                        @endif
+                    </td>
+                    <td>
                         {!! Form::open(['route' => ['participants.destroy', $participant->id], 'method' => 'delete']) !!}
                         <div class='btn-group'>
                             <a href="{{ route('participants.edit', [$participant->id]) }}"

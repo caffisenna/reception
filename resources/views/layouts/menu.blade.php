b39c7b0c558329e6dba6231199d0ff8fbfb9715a
caffi <caffi.senna@gmail.com>
2022-11-18 22:20:12 +0900

    feature#37
    close #37

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,51 +1,44 @@
 @if (auth()->user()->is_admin)
     <p class="uk-text-warning">管理業務</p>
     <li class="nav-item">
-        <a href="{{ route('participants.index') }}" class="nav-link {{ Request::is('participants*') ? 'active' : '' }}">
-            <p>参加者</p>
+        <a href="{{ route('participants.index') }}" class="nav-link {{ Request::is('*participants*') ? 'active' : '' }}">
+            <p>参加者リスト</p>
         </a>
     </li>
+
     <li class="nav-item">
-        <a href="{{ url('/admin/sendmail') }}" class="nav-link {{ Request::is('*sendmail') ? 'active' : '' }}">
-            <p>デジパス送信(個別)</p>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="{{ url('/admin/sendmail_pref') }}" class="nav-link {{ Request::is('*sendmail_pref') ? 'active' : '' }}">
-            <p>デジパス送信(県連単位)</p>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="{{ url('/admin/checked_in') }}" class="nav-link {{ Request::is('/admin/checked_in') ? 'active' : '' }}">
+        <a href="{{ url('/admin/checked_in') }}" class="nav-link {{ Request::is('*admin/checked_in') ? 'active' : '' }}">
             <p>チェックイン済み</p>
         </a>
     </li>
     <li class="nav-item">
         <a href="{{ url('/admin/not_checked_in') }}"
-            class="nav-link {{ Request::is('/admin/not_checked_in') ? 'active' : '' }}">
+            class="nav-link {{ Request::is('*admin/not_checked_in') ? 'active' : '' }}">
             <p>未チェックイン</p>
         </a>
     </li>
     <li class="nav-item">
         <a href="{{ url('/admin/absent_list') }}"
-            class="nav-link {{ Request::is('/admin/absent_list') ? 'active' : '' }}">
+            class="nav-link {{ Request::is('*admin/absent_list') ? 'active' : '' }}">
             <p>欠席リスト</p>
         </a>
     </li>
     <li class="nav-item">
-        <a href="{{ url('/admin/cancel') }}" class="nav-link {{ Request::is('/admin/cancel*') ? 'active' : '' }}">
+        <a href="{{ url('/admin/cancel') }}" class="nav-link {{ Request::is('*admin/cancel*') ? 'active' : '' }}">
             <p>取消処理</p>
         </a>
     </li>
-
+    <p class="uk-text-warning">座席情報</p>
     <li class="nav-item">
-        <a href="{{ url('/admin/seat_number') }}" class="nav-link {{ Request::is('*/seat_number*') ? 'active' : '' }}">
+        <a href="{{ url('/admin/seat_number') }}"
+            class="nav-link {{ Request::is('*/seat_number*') ? 'active' : '' }}">
             <p>式典座席</p>
         </a>
     </li>
 
     <li class="nav-item">
-        <a href="{{ url('/admin/reception_seat_number') }}" class="nav-link {{ Request::is('*/reception_seat_number*') ? 'active' : '' }}">
+        <a href="{{ url('/admin/reception_seat_number') }}"
+            class="nav-link {{ Request::is('*/reception_seat_number*') ? 'active' : '' }}">
             <p>レセプション座席</p>
         </a>
     </li>
@@ -54,8 +47,23 @@
             class="nav-link" target="_blank">隣接シート特定<span uk-icon="icon: google"></span></a>
     </li>
 
+    <p class="uk-text-warning">メール送信</p>
     <li class="nav-item">
-        <a href="{{ url('/admin/fee_check') }}" class="nav-link {{ Request::is('/admin/fee_check*') ? 'active' : '' }}">
+        <a href="{{ url('/admin/sendmail') }}" class="nav-link {{ Request::is('*sendmail') ? 'active' : '' }}">
+            <p>デジパス送信(個別)</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="{{ url('/admin/sendmail_pref') }}"
+            class="nav-link {{ Request::is('*sendmail_pref') ? 'active' : '' }}">
+            <p>デジパス送信(県連単位)</p>
+        </a>
+    </li>
+
+    <p class="uk-text-warning">参加費</p>
+    <li class="nav-item">
+        <a href="{{ url('/admin/fee_check') }}"
+            class="nav-link {{ Request::is('*admin/fee_check*') ? 'active' : '' }}">
             <p>レセプション参加費</p>
         </a>
     </li>
@@ -63,7 +71,7 @@
     <p class="uk-text-warning">情報</p>
     <li class="nav-item">
         <a href="{{ route('admin_staffinfos.index') }}"
-            class="nav-link {{ Request::is('admin_staffinfos*') ? 'active' : '' }}">
+            class="nav-link {{ Request::is('*admin_staffinfos*') ? 'active' : '' }}">
             <p>スタッフ情報</p>
         </a>
     </li>
